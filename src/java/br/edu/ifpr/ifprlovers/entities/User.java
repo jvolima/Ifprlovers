646c006c7da03976044d917a9bd6a22dc0469288
João Vitor Lima <jvolima2004@gmail.com>
2022-06-26 21:00:13 -0300

    tela de cadastro

---
+++ src/java/br/edu/ifpr/ifprlovers/entities/User.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifpr.ifprlovers.entities;
+
+/**
+ *
+ * @author jvolima
+ */
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String gender;
+    private String sexualOrientation;
+    private int age;
+    
+    public User(int id, String name, String email, String password, String gender, String sexualOrientation, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.sexualOrientation = sexualOrientation;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getSexualOrientation() {
+        return sexualOrientation;
+    }
+
+    public void setSexualOrientation(String sexualOrientation) {
+        this.sexualOrientation = sexualOrientation;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
